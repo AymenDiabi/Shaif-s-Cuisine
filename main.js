b2d0df1348a4242a9eed20b6b8e1a6060c1bdc5d
Aymen Diabi <aymendiabi@gmail.com>
2022-09-08 00:53:18 +0100

    Finished the navigation

---
+++ main.js
@@ -0,0 +1,22 @@
+const navIconEl = document.querySelector('.nav__icon');
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav-list');
+const navBgOverlayEl = document.querySelector('.bg-overlay');
+
+const navOpen = () => {
+  navList.classList.add('show');
+  navBgOverlayEl.classList.add('active');
+
+  document.body.style='visibility: visible; height: 100vh; width: 100vw; overflow: hidden;';
+}
+
+const navClose = () => {
+  navList.classList.remove('show');
+  navBgOverlayEl.classList.remove('active');
+
+  document.body.style='visiblity: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+navIconEl.addEventListener('click', navOpen);
+navCloseEl.addEventListener('click', navClose);
+navBgOverlayEl.addEventListener('click', navClose);
